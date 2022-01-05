5d3a82af020d95e0b9f085c148648c480181a619
AsP3X <zombymedia@gmail.com>
2022-01-05 17:44:52 +0100

    TASK: updated to latest release

--- 1.18.1/run.sh
+++ 1.18.1/run.sh
@@ -29,7 +29,7 @@ if test -f "$JARFILE"; then
 	screen -S Minecraft-Server /bin/sh -c "java -Xmx${RAM} -Xms${RAM} -jar papermc.jar"
 else
 	echo "Creating new Files"
-	wget https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/101/downloads/paper-1.18.1-101.jar -O /temp/papermc.jar
+	wget https://papermc.io/api/v2/projects/paper/versions/1.18.1/builds/133/downloads/paper-1.18.1-133.jar -O /temp/papermc.jar
 	touch /temp/eula.txt
 	echo "eula=true" > /temp/eula.txt
 	sed -i -e 's/false/true/g' /temp/eula.txt
