88c682ff93a75b4d38fedb103cbf7410dda9936a
AsP3X <zombymedia@gmail.com>
2022-03-06 15:21:42 +0100

    TASK: added new 1.18.2 version to the build actions

---
+++ 1.18.2/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+####################################################################
+## ZOUPA - (ZombyMediaIC open source usage protection agreement)  ##
+## License as of: 19.07.2021 10:32 | #202107191032                ##
+## Niklas Vorberg (AsP3X)                                         ##
+####################################################################
+
+# echo "###############################################"
+# echo "###############################################"
+# echo "       Warning this build is deprecated        "
+# echo "  and will no longer be updated and supported  "
+# echo "                                               "
+# echo "     Do not under ANY CIRCUMSTANCES use it     "
+# echo "     for production or anything else exept     "
+# echo "       testing and private purposes only       "
+# echo "###############################################"
+# echo "###############################################"
+
+sleep 4
+
+if [[ "${RAM}" ]]; then
+	echo "Using ${RAM}"
+else
+	echo "Set ram to default of 4G"
+	echo "This can be changed later"
+	RAM=4G
+fi
+
+JARFILE=/server/papermc.jar
+if test -f "$JARFILE"; then
+	screen -S Minecraft-Server /bin/sh -c "java -Xmx${RAM} -Xms${RAM} -jar papermc.jar"
+else
+	echo "Creating new Files"
+	wget https://papermc.io/api/v2/projects/paper/versions/1.18.2/builds/231/downloads/paper-1.18.2-231.jar -O /temp/papermc.jar
+	touch /temp/eula.txt
+	echo "eula=true" > /temp/eula.txt
+	sed -i -e 's/false/true/g' /temp/eula.txt
+	cp /temp/papermc.jar /server
+	cp /temp/eula.txt /server
+	cp /temp/run.sh /server
+	sleep 1
+	screen -S Minecraft-Server /bin/sh -c "java -Xmx${RAM} -Xms${RAM} -jar papermc.jar"
+fi
